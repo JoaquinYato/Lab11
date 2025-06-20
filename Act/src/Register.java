2c1d2c1c6e855e38813705d2c0564774ebc2b0dc
Joaquin Yato <joaquin.yato@ucsm.edu.pe>
2025-06-19 23:03:14 -0500

    Creacion de la clase Rgister

---
+++ Act/src/Register.java
@@ -0,0 +1,17 @@
+public class Register {
+    private in key;
+    private String name;
+
+    public Register(int key, String name){
+        this.key=key;
+        this.name=name;
+    }
+
+    public int getKey(){return key;}
+
+    public String getName){return name;}
+    public String toString({
+        return "(" + jey + ", " +name+ ")";
+    })
+
+}
